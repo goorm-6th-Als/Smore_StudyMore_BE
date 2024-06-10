92c1eb81997a206b101383620e59ff66a03a2357
남수연 <53739820+namtndus@users.noreply.github.com>
2024-06-10 12:18:43 +0900

    feat: GXK-37 Member 엔티티 변경

--- src/main/java/com/als/SMore/domain/entity/Member.java
+++ src/main/java/com/als/SMore/domain/entity/Member.java
@@ -20,9 +20,6 @@ public class Member {
     @Column(name = "user_id", nullable = false)
     private String userId;
 
-    @Column(name = "user_password", nullable = false)
-    private String userPassword;
-
     @Column(name = "profile_img")
     private String profileImg;
 
@@ -31,4 +28,7 @@ public class Member {
 
     @Column(name = "full_name", nullable = false)
     private String fullName;
+
+    @Column(name = "kakao_user_id")
+    private String KakaoUserId;
 }
