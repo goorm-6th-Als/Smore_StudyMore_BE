852054f2d5f306cabce29cfcd2bd4a649cf2f292
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-06-12 00:41:04 +0900

    [feat] GXK-82 스터디 게시물 검색 API (modify 순으로 정렬)

--- src/main/java/com/als/SMore/study/studyCRUD/service/StudyBoardService.java
+++ src/main/java/com/als/SMore/study/studyCRUD/service/StudyBoardService.java
@@ -18,8 +18,7 @@ public class StudyBoardService {
     private final StudyBoardRepository studyBoardRepository;
 
     /**
-     * 모든 StudyBoard 엔티티를 조회하여 DTO 리스트로 반환합니다.
-     *
+     * 모든 StudyBoard 조회
      * @return 모든 StudyBoardDTO 리스트
      */
     public List<StudyBoardDTO> getAllStudyBoards() {
@@ -29,8 +28,7 @@ public class StudyBoardService {
     }
 
     /**
-     * ID로 StudyBoard 엔티티를 조회하여 DTO로 반환합니다.
-     *
+     * 스터디 게시물 상세 조회하기.
      * @param id 조회할 StudyBoard의 ID
      * @return 조회된 StudyBoardDTO
      */
@@ -41,13 +39,23 @@ public class StudyBoardService {
     }
 
     /**
-     * Pageable 객체를 사용하여 StudyBoard 엔티티의 페이지를 조회하고, DTO 페이지로 반환합니다.
-     *
+     * StudyBoard  페이징
      * @param pageable 페이지 정보
      * @return StudyBoardDTO의 페이지 객체
      */
     public Page<StudyBoardDTO> getStudyBoardsPage(Pageable pageable) {
         return studyBoardRepository.findAll(pageable)
+                .map(studyBoard -> new StudyBoardDTO(studyBoard, false));
+    }
+
+    /**
+     * 스터디 보드 검색 기능 수정날짜로 정렬
+     * @param keyword 검색할 문자열
+     * @param pageable 페이지 정보
+     * @return 검색된 StudyBoardDTO의 페이지 객체
+     */
+    public Page<StudyBoardDTO> searchStudyBoards(String keyword, Pageable pageable) {
+        return studyBoardRepository.searchByAdTitle(keyword, pageable)
                 .map(studyBoard -> new StudyBoardDTO(studyBoard, false));
     }
 }
