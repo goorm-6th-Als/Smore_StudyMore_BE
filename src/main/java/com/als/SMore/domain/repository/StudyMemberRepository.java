8d0919dce34d198ac977c6c31f89fefe596a7973
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-06-12 15:06:59 +0900

    [feat] GXK-19 출석(시작, 퇴장)

--- src/main/java/com/als/SMore/domain/repository/StudyMemberRepository.java
+++ src/main/java/com/als/SMore/domain/repository/StudyMemberRepository.java
@@ -1,9 +1,14 @@
 package com.als.SMore.domain.repository;
 
+import com.als.SMore.domain.entity.Member;
+import com.als.SMore.domain.entity.Study;
 import com.als.SMore.domain.entity.StudyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> {
+    Optional<StudyMember> findByMemberAndStudy(Member member, Study study);
 }
