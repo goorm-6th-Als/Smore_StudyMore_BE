b7cf957894be25a5e9c88e4ee63267f5a1f5eeaf
JulietSierra <108870712+Kangjunesu@users.noreply.github.com>
2024-06-12 14:55:30 +0900

    [feat] GXK-64 공지사항 끝, Global Exception Handler 적용

---
+++ src/main/java/com/als/SMore/global/CustomErrorCode.java
@@ -0,0 +1,29 @@
+package com.als.SMore.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CustomErrorCode {
+    CustomErrorCode(HttpStatus.BAD_REQUEST,"이미 존재하는 ID입니다."),
+    INVALID_NICKNAME(HttpStatus.BAD_REQUEST,"이미 존재하는 닉네임입니다."),
+    UNKNOWN_PASSWORD(HttpStatus.BAD_REQUEST ,"기존 비밀번호를 확인해주세요."),
+    ERROR_PASSWORD(HttpStatus.BAD_REQUEST,"비밀번호가 일치하지 않습니다."),
+    ERROR_USER(HttpStatus.BAD_REQUEST,"해당하는 유저가 없습니다."),
+    NOT_SAME_PASSWORD(HttpStatus.BAD_REQUEST,"동일한 비밀번호로 변경할 수 없습니다."),
+    INVALID_USERID(HttpStatus.BAD_REQUEST,"이미 존재하는 아이디입니다"),
+
+
+    //notice
+    NOT_EXIST_PAGE(HttpStatus.NOT_FOUND, "존재하지 않는 게시물 입니다."),
+    INVALID_TITLE_NAME(HttpStatus.BAD_REQUEST, "제목은 공백 포함 1자 이상 30자 이하로 해주세요.");
+
+
+
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
--- src/main/java/com/als/SMore/study/notice/service/NoticeBoardService.java
+++ src/main/java/com/als/SMore/study/notice/service/NoticeBoardService.java
@@ -3,16 +3,15 @@ package com.als.SMore.study.notice.service;
 import com.als.SMore.domain.entity.NoticeBoard;
 import com.als.SMore.domain.entity.Study;
 import com.als.SMore.domain.repository.NoticeBoardRepository;
-import com.als.SMore.domain.repository.StudyRepository;
 import com.als.SMore.study.notice.DTO.MessageResponseDTO;
 import com.als.SMore.study.notice.DTO.NoticeRequestDTO;
 import com.als.SMore.study.notice.DTO.NoticeResponseDTO;
+import com.als.SMore.study.notice.validator.NoticeValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,53 +19,48 @@ import java.util.NoSuchElementException;
 public class NoticeBoardService {
 
     private final NoticeBoardRepository noticeBoardRepository;
-    private final StudyRepository studyRepository;
+    private final NoticeValidator noticeValidator;
 
     @Transactional(readOnly = true)
     public NoticeResponseDTO getNotice(Long studyPK, Long noticeBoardPK) {
-        Study study = findStudyByStudyPk(studyPK);
-        NoticeBoard noticeBoard = findNoticeByBoardPkAndStudy(noticeBoardPK, study);
+        Study study = noticeValidator.findStudyByStudyPk(studyPK);
+        NoticeBoard noticeBoard = noticeValidator.findNoticeByBoardPkAndStudy(noticeBoardPK, study);
         return new NoticeResponseDTO(noticeBoard);
     }
 
     @Transactional(readOnly = true)
     public List<NoticeBoard> getAllNotice(Long studyPK) {
-        Study study = findStudyByStudyPk(studyPK);
+        Study study = noticeValidator.findStudyByStudyPk(studyPK);
         return noticeBoardRepository.findAllByStudy(study);
     }
 
     public NoticeResponseDTO createNotice(Long studyPK, NoticeRequestDTO requestDTO) {
         //연관관계 주인 = noticeBoard (O) Study(X)
         //유저 정보 받아오면, 유저 정보 + 스터디 PK로 방장이 맞는지 확인하도록.
-        Study study = findStudyByStudyPk(studyPK);
+        noticeValidator.validateTitleLength(requestDTO.getNoticeTitle());
+
+        Study study = noticeValidator.findStudyByStudyPk(studyPK);
         NoticeBoard noticeBoard = noticeBoardRepository.saveAndFlush(new NoticeBoard(requestDTO,study));
         return new NoticeResponseDTO(noticeBoard);
     }
 
     public NoticeResponseDTO updateNotice(Long studyPK, Long noticeBoardPK, NoticeRequestDTO requestDTO) {
         //유저 정보 받아오면, 유저 정보 + 스터디 PK로 방장이 맞는지 확인하도록.
-        Study study = findStudyByStudyPk(studyPK);
-        NoticeBoard noticeBoard = findNoticeByBoardPkAndStudy(noticeBoardPK, study);
+        noticeValidator.validateTitleLength(requestDTO.getNoticeTitle());
+
+        Study study = noticeValidator.findStudyByStudyPk(studyPK);
+        NoticeBoard noticeBoard = noticeValidator.findNoticeByBoardPkAndStudy(noticeBoardPK, study);
         noticeBoard.updateNotice(requestDTO);
 
         return new NoticeResponseDTO(noticeBoardPK, requestDTO);
     }
 
     public MessageResponseDTO deleteNotice(Long studyPK, Long noticeBoardPK) {
-        Study study = findStudyByStudyPk(studyPK);
-        NoticeBoard noticeBoard = findNoticeByBoardPkAndStudy(noticeBoardPK, study);
+        Study study = noticeValidator.findStudyByStudyPk(studyPK);
+        NoticeBoard noticeBoard = noticeValidator.findNoticeByBoardPkAndStudy(noticeBoardPK, study);
 
         noticeBoardRepository.delete(noticeBoard);
         return new MessageResponseDTO("삭제 완료");
     }
 
-    private Study findStudyByStudyPk(Long studyPK) {
-        return studyRepository.findById(studyPK)
-                .orElseThrow(NoSuchElementException::new);
-    }
-
-    private NoticeBoard findNoticeByBoardPkAndStudy(Long noticeBoardPK, Study study) {
-        return noticeBoardRepository.findByNoticeBoardPkAndStudy(noticeBoardPK, study)
-                .orElseThrow(NoSuchElementException::new);
-    }
 }
