2ccbc77f4062a38842b03974aa989e4dd750b67f
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-06-14 16:45:44 +0900

    test update

---
+++ src/main/java/com/als/SMore/study/attendance/DTO/request/LearningMonthRequestDTO.java
@@ -0,0 +1,15 @@
+package com.als.SMore.study.attendance.DTO.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LearningMonthRequestDTO {
+    private int year;
+    private int month;
+}
