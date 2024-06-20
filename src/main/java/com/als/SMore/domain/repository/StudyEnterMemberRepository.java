f8842f70506183ca7c076fe5e30e8540e1114dca
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-06-20 15:58:53 +0900

    Revert "[feat]: GXK-84 스터디 목록의 요소인 StudyResponse 파일 구현 (#31)"
    This reverts commit 8a156c112697f4b2775954b1c0f0854163ba97f0.

--- src/main/java/com/als/SMore/domain/repository/StudyEnterMemberRepository.java
+++ src/main/java/com/als/SMore/domain/repository/StudyEnterMemberRepository.java
@@ -15,4 +15,5 @@ public interface StudyEnterMemberRepository extends JpaRepository<StudyEnterMemb
     Optional<StudyEnterMember> deleteStudyEnterMemberByMember_UserIdAndStudy_StudyPk (String userId,Long studyPk);
     Optional<StudyEnterMember> findStudyEnterMemberByMember_UserIdAndStudy_StudyPk (String userId,Long studyPk);
     List<StudyEnterMember> findAllByStudy_StudyPk(Long studyPk);
+
 }
