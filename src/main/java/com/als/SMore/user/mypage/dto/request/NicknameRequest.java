126e05fad8996be529cd34d0b72f3f542977cbfb
남수연 <53739820+namtndus@users.noreply.github.com>
2024-06-21 15:32:46 +0900

    [chore] 패키지 구조 변경

---
+++ src/main/java/com/als/SMore/user/mypage/dto/request/NicknameRequest.java
@@ -0,0 +1,12 @@
+package com.als.SMore.user.mypage.dto.request;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NicknameRequest {
+
+    private String nickname;
+}
