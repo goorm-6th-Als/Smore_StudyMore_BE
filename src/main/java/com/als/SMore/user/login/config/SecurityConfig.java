ca63f5ccdbd05091c829332b0c175e35f5681ce5
남수연 <53739820+namtndus@users.noreply.github.com>
2024-06-25 15:58:25 +0900

    [refactor] SecurityConfig 에서 필요없는 코드 삭제

--- src/main/java/com/als/SMore/user/login/config/SecurityConfig.java
+++ src/main/java/com/als/SMore/user/login/config/SecurityConfig.java
@@ -52,21 +52,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration config = new CorsConfiguration();
-//
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(List.of("http://localhost:3000"));
-//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-//        config.setAllowedHeaders(List.of("*"));
-//        config.setExposedHeaders(List.of("*"));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
-
     @Bean
     public CorsConfigurationSource corsConfiguration() {
         CorsConfiguration config = new CorsConfiguration();
