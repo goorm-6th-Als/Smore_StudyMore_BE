bdc471bb514c775e9d71f5956b12db8bfe89b40b
남수연 <53739820+namtndus@users.noreply.github.com>
2024-07-02 17:42:11 +0900

    [feat] GXK-102 토큰 예외 처리

---
+++ src/main/java/com/als/SMore/user/login/util/CustomAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.als.SMore.user.login.util;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        // 토큰 인증 후 권한 거부
+        log.info("권한 거부");
+    }
+}
