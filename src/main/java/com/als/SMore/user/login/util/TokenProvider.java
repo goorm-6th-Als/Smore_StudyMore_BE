7f2721bcc5fb734db082fda2ba47c5f179fce27c
남수연 <53739820+namtndus@users.noreply.github.com>
2024-07-02 17:48:31 +0900

    [feat] GXK-103 새로운 토큰 생성 구현

--- src/main/java/com/als/SMore/user/login/util/TokenProvider.java
+++ src/main/java/com/als/SMore/user/login/util/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.als.SMore.user.login.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -66,16 +67,60 @@ public class TokenProvider {
 
     public String validate(String jwt) {
         String subject = null;
-        try {
             subject = Jwts.parser().verifyWith(key)
                     .build().parseSignedClaims(jwt)
                     .getPayload().getSubject();
-        } catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
 
         return subject;
+    }
+
+    // 기존의 jwt를 받아서 다른 jwt를 생성하는 기능
+    public String createRenewToken(String jwt, Long studyPk, String role){
+        // jwt를 분해함
+        Claims payload = Jwts.parser().verifyWith(key)
+                .build().parseSignedClaims(jwt)
+                .getPayload();
+
+        Map<String,String> roleList = new HashMap<>();
+
+        Set<String> keySet = payload.keySet();
+        for (String key : keySet) {
+            if(!key.equals("sub") && !key.equals("iat") && !key.equals("exp")){
+                System.out.println(key);
+                roleList.put(key, (String) payload.get(key));
+            }
+
+        }
+
+        roleList.put(String.valueOf(studyPk),role);
+
+        // 새로운 토큰 발급
+        String renewToken = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .subject(payload.getSubject())
+                .issuedAt(payload.getIssuedAt())
+                .claims(roleList)  // 오브젝트 타입으로 넣기
+                .expiration(payload.getExpiration())
+                .compact();
+        return renewToken;
+    }
+
+    // 토큰에 studyPk에 대한 역할이 있는지 확인하는 함수
+    public boolean isCheckedRole(String jwt, String studyPk){
+        Claims payload = Jwts.parser().verifyWith(key)
+                .build().parseSignedClaims(jwt)
+                .getPayload();
+        if (payload.get(studyPk) == null){
+            return false;
+        }
+        return true;
+    }
+
+    public String getRole(String jwt, String studyPk){
+        String role = (String) Jwts.parser().verifyWith(key)
+                .build().parseSignedClaims(jwt)
+                .getPayload().get(studyPk);
+        return role;
     }
 
 }
