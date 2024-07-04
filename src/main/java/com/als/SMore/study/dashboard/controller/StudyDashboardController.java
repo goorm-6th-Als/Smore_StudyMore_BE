28b2341cedbef85ff8c2b67ae90bdd624e1ea8d9
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-07-04 20:51:49 +0900

    [feat] GXK-74 월별 출석 현황 API 추가

--- src/main/java/com/als/SMore/study/dashboard/controller/StudyDashboardController.java
+++ src/main/java/com/als/SMore/study/dashboard/controller/StudyDashboardController.java
@@ -1,15 +1,18 @@
 package com.als.SMore.study.dashboard.controller;
 
-import com.als.SMore.study.dashboard.DTO.AttendanceStatusDTO;
+import com.als.SMore.study.dashboard.DTO.MonthlyAttendanceStatusDTO;
+import com.als.SMore.study.dashboard.DTO.TodayAttendanceStatusDTO;
 import com.als.SMore.study.dashboard.DTO.StudyMemberDTO;
 import com.als.SMore.study.dashboard.DTO.StudyRankingDTO;
 import com.als.SMore.study.dashboard.service.StudyDashboardService;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,12 +30,13 @@ public class StudyDashboardController {
     @GetMapping("/members")
     public ResponseEntity<List<StudyMemberDTO>> getStudyMembers(
             @PathVariable Long studyPk) {
-        List<StudyMemberDTO> studyMembers = studyDashboardService.getStudyMembers(studyPk);
+        List<StudyMemberDTO> studyMembers = studyDashboardService.getAllStudyMembers(studyPk);
         return ResponseEntity.ok(studyMembers);
     }
 
     /**
      * 스터디 content 조회
+     *
      * @param studyPk 스터디 PK
      * @return 스터디 content와 함께 응답
      */
@@ -56,14 +60,34 @@ public class StudyDashboardController {
     }
 
     /**
-     * 스터디 멤버 출석 현황 조회
+     * 스터디 멤버 출석 현황 조회 (오늘)
      *
      * @param studyPk 스터디 PK
      * @return StudyRankingDTO 리스트와 함께 OK 응답 반환
      */
-    @GetMapping
-    public ResponseEntity<List<AttendanceStatusDTO>> getAttendanceStatus(@PathVariable Long studyPk) {
-        List<AttendanceStatusDTO> attendanceStatus = studyDashboardService.getAttendanceStatus(studyPk);
+    @GetMapping("/attendance/today")
+    public ResponseEntity<List<TodayAttendanceStatusDTO>> getTodayAttendanceStatus(@PathVariable Long studyPk) {
+        List<TodayAttendanceStatusDTO> attendanceStatus = studyDashboardService.getTodayAttendanceStatus(studyPk);
         return ResponseEntity.ok(attendanceStatus);
     }
+
+
+    /**
+     * 월별 스터디 출석 현황 조회
+     *
+     * @param studyPk   스터디 PK
+     * @param year
+     * @parm month 조회할 연월
+     * @return 월별 출석 현황 리스트와 함께 OK 응답 반환
+     */
+    @GetMapping("/attendance/monthly")
+    public ResponseEntity<Map<Integer, List<MonthlyAttendanceStatusDTO>>> getMonthlyAttendanceStatus(
+            @PathVariable Long studyPk,
+            @RequestParam int year,
+            @RequestParam int month) {
+        Map<Integer, List<MonthlyAttendanceStatusDTO>> monthlyAttendanceStatus = studyDashboardService.getMonthlyAttendanceStatus(
+                studyPk, year, month);
+        return ResponseEntity.ok(monthlyAttendanceStatus);
+    }
+
 }
