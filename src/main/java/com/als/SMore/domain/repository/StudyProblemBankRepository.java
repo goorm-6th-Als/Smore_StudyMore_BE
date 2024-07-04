46d69ac4e41dd1a92919ec24568b5fac95b31bee
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-07-04 20:50:14 +0900

    [style] 출석, 문제 주석 추가 [refactor] 문제은행 mapper 추가 [feat] 내 문제은행 불러오기 추가(스터디) [refactor] 문제 만들기에 pk 반환

--- src/main/java/com/als/SMore/domain/repository/StudyProblemBankRepository.java
+++ src/main/java/com/als/SMore/domain/repository/StudyProblemBankRepository.java
@@ -1,5 +1,6 @@
 package com.als.SMore.domain.repository;
 
+import com.als.SMore.domain.entity.Member;
 import com.als.SMore.domain.entity.Study;
 import com.als.SMore.domain.entity.StudyProblemBank;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface StudyProblemBankRepository extends JpaRepository<StudyProblemBank, Long> {
     List<StudyProblemBank> findByStudyOrderByStudyProblemBankPkDesc(Study study);
+    List<StudyProblemBank> findByMemberAndStudy(Member member, Study study);
     long countByStudy(Study study);
 }
