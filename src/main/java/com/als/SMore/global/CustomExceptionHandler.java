edc7fe9562839e7a62b06de38e5b6bf6c721c939
남수연 <53739820+namtndus@users.noreply.github.com>
2024-07-09 00:38:19 +0900

    [fix] 틀린 URI와 다른 예외 처리 핸들러 처리 구현

--- src/main/java/com/als/SMore/global/CustomExceptionHandler.java
+++ src/main/java/com/als/SMore/global/CustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.als.SMore.global;
 
+import com.als.SMore.user.mypage.dto.response.MessageResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -15,5 +17,15 @@ public class CustomExceptionHandler {
     @ExceptionHandler(JwtAuthException.class)
     protected ResponseEntity<ErrorResponse> handlerJwtAuthException(JwtAuthException jwt){
         return ErrorResponse.toResponse(jwt);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ResponseEntity<MessageResponse> handlerNoHandlerFoundException(){
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("잘못된 URI 입니다.").build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(){
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("오류가 발생하였습니다").build());
     }
 }
