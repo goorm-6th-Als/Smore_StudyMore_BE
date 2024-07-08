fb7ae4c1533277af5950da9cbd0c8337a42df61a
남수연 <53739820+namtndus@users.noreply.github.com>
2024-07-09 01:44:53 +0900

    [fix] 틀린 URI와 다른 예외 처리 핸들러 처리 구현

--- src/main/java/com/als/SMore/global/CustomExceptionHandler.java
+++ src/main/java/com/als/SMore/global/CustomExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.als.SMore.global;
 
 import com.als.SMore.user.mypage.dto.response.MessageResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+@Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
@@ -20,12 +23,20 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    protected ResponseEntity<MessageResponse> handlerNoHandlerFoundException(){
+    protected ResponseEntity<MessageResponse> handlerNoHandlerFoundException(NoHandlerFoundException e){
+        log.error(e.getMessage());
         return ResponseEntity.badRequest().body(MessageResponse.builder().message("잘못된 URI 입니다.").build());
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(){
+    protected ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(Exception e){
+        log.error(e.getMessage());
         return ResponseEntity.badRequest().body(MessageResponse.builder().message("오류가 발생하였습니다").build());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<MessageResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("메소드 함수가 잘못 설정되었습니다").build());
     }
 }
