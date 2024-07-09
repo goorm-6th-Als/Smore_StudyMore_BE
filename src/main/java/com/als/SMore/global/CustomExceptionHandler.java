6f1863d0fde13c042363673d0045a95ebc41d138
남수연 <53739820+namtndus@users.noreply.github.com>
2024-07-09 14:32:38 +0900

    [fix] 발생한 예외들을 세분화하여 처리

--- src/main/java/com/als/SMore/global/CustomExceptionHandler.java
+++ src/main/java/com/als/SMore/global/CustomExceptionHandler.java
@@ -3,9 +3,16 @@ package com.als.SMore.global;
 import com.als.SMore.user.mypage.dto.response.MessageResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
@@ -22,6 +29,7 @@ public class CustomExceptionHandler {
         return ErrorResponse.toResponse(jwt);
     }
 
+    // 없는 URI 에 접근 할시 발생하는 예외를 처리
     @ExceptionHandler(NoHandlerFoundException.class)
     protected ResponseEntity<MessageResponse> handlerNoHandlerFoundException(NoHandlerFoundException e){
         log.error(e.getMessage());
@@ -30,13 +38,57 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(Exception e){
+        e.printStackTrace();
         log.error(e.getMessage());
         return ResponseEntity.badRequest().body(MessageResponse.builder().message("오류가 발생하였습니다").build());
     }
 
+    // HTTP 메소드가 잘못 설정되어 요청하는 경우, 예외를 처리
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<MessageResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
         log.error(e.getMessage());
         return ResponseEntity.badRequest().body(MessageResponse.builder().message("메소드 함수가 잘못 설정되었습니다").build());
+    }
+
+    // HTTP의 RequestBody가 누락된 경우
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected ResponseEntity<MessageResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("파라미터가 누락되었습니다").build());
+    }
+
+    // 파라미터의 이름과 변수의 이름이 일치하지 않을 때 발생하는 오류
+    @ExceptionHandler(MissingPathVariableException.class)
+    protected ResponseEntity<MessageResponse> handleMissingPathVariableException(MissingPathVariableException e){
+        log.error(e.getMessage());
+        return ResponseEntity.internalServerError().body(MessageResponse.builder().message("파라미터의 이름과 변수의 이름이 일치하지 않습니다").build());
+    }
+
+    // 값이 들어오지 않았을 때 발생하는 오류
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<MessageResponse> handleMissingPathVariableException(HttpMessageNotReadableException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("데이터가 전달이 되지 않았습니다").build());
+    }
+
+    // pathVariable 에 원하는 자료형이 들어오지 않을 발생하는 오류 ex> Long(기대) -> String(들어온 값)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<MessageResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("타입이 일치하지 않습니다").build());
+    }
+
+    // 전달받은 데이터의 타입이 multipart/form-data이 아닐때 발생하는 오류
+    @ExceptionHandler(MultipartException.class)
+    protected ResponseEntity<MessageResponse> handleMultipartException(MultipartException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("content-type이 multipart/form-data 가 아닙니다").build());
+    }
+
+    // 이미지 파일을 받을때, json 파일도 같이 받을 경우, json 파일을 content-type을 명시해지 않을 경우 발생하는 오류
+    @ExceptionHandler(HttpMediaTypeException.class)
+    protected ResponseEntity<MessageResponse> handleHttpMediaTypeException(HttpMediaTypeException e){
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest().body(MessageResponse.builder().message("json 데이터에 content-type을 application/json 으로 추가해주세요").build());
     }
 }
